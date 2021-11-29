debe181654358ac1232072e7680e85b9adc0406b
muratkabia <kaanabin4141@gmail.com>
2021-11-29 16:39:04 +0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import webbrowser
 
 _currentDegree = 0
 _heaterStatus = False
@@ -6,21 +7,36 @@ _altitute = 0
 
 # get keyboard key from user
 def get_key():
-    key = input("Enter key: ")
+    key = input("Enter key: ").lower()
     if key == "":
         return None
     else:
         return key
+        
+def getKeyAndRun():
+    # check if get_key is A
+    if get_key() == "a":
+        print("Currrent degree: ", _currentDegree)
+        print("Heater status: ", _heaterStatus)
+        print("Altitute: ", _altitute)
+    #else check if get_key is Esc
+    elif get_key() == "q":
+        sys.exit()
+    #else check if get_key is W then open a website in browser
+    elif get_key() == "w":
+        webbrowser.open("https://github.com/muratkabia/ColdDrone")
 
-# check if get_key is A
-if get_key() == "A":
-    print("Currrent degree: ", _currentDegree)
-    print("Heater status: ", _heaterStatus)
-    print("Altitute: ", _altitute)
-#else check if get_key is Esc
-elif get_key() == "Esc":
-    sys.exit()
-#else check if get_key is W then open a website in browser
-elif get_key() == "W":
-    import webbrowser
-    webbrowser.open("http://www.google.com")
+
+# main function greeting
+def main():
+    print("Welcome to ColdDrone")
+    print("Press A to get information")
+    print("Press Esc to exit")
+    print("Press W to open website")
+    while True:
+        getKeyAndRun()
+    
+
+# if __name__ is "__main__" run main()
+if __name__ == "__main__":
+    main()
